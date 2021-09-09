7fc2c423cb1e2a0ba0bdbf950f78f1af1aaddaa3
Ben Newsome <benrogersnewsome@gmail.com>
2021-09-09 21:08:18 +0100

    Started work on tests

---
+++ src/__tests__/Typography.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+
+import { Heading} from "../typography/Typography";
+
+describe("<Heading>Hello World</Heading>", () => {
+  test("should display a blank login form, with remember me checked by default", async () => {
+    
+  });
+});
