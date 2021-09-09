52bbe7afc2a503412f54cbfcbfc82ebd7b50b3f6
Ben Newsome <benrogersnewsome@gmail.com>
2021-09-09 21:07:48 +0100

    Added basic heading functionality

--- src/typography/Typography.tsx
+++ src/typography/Typography.tsx
@@ -1,31 +1,57 @@
 import styled from 'styled-components'
 import React from 'react'
 
-const ResponsiveResizer = styled.span`
+type ResponsiveResizerProps = {
+    lowerResizeLimit: number,
+    upperResizeLimit: number,
+    emToPx: number
+}
+
+/** 
+ * Treat 1em as the base size at 1000px
+*/
+const ResponsiveResizer = styled.span<ResponsiveResizerProps>`
     font-size: 1em;
 
-    @media screen and (min-width: 320px) {
+    @media screen and (min-width: ${props => props.lowerResizeLimit}px) {
         html {
-            font-size: calc(1em + 6 * ((100vw - 320px) / 680));
+            font-size: calc(1em - ${props => props.emToPx/2} * (${props => props.upperResizeLimit}px - 100vw) / (${props => props.upperResizeLimit} - ${props => props.lowerResizeLimit}));
         }
     }
-    @media screen and (min-width: 1000px) {
+    @media screen and (min-width: ${props => props.upperResizeLimit}px) {
         html {
-            font-size: 22px;
+            font-size: 1em;
         }
     } 
 `
 
-type HeadingProps = {
-    className?: string
+// ResponsiveResizer.defaultProps = {
+//     lowerResizeLimit: 320,
+//     upperResizeLimit: 1000
+// }
+
+type HeadingProps = ResponsiveResizerProps & {
+    lowerResizeLimit?: number,
+    upperResizeLimit?: number,
+    className?: string,
+    children: React.ReactNode
 }
 
-const Heading : React.FC<HeadingProps> = ({ className }) => {
-    return(
-        <h1 className={className}>
-            <span>
+export const Heading : React.FC<HeadingProps> = ({ className, children, lowerResizeLimit=320, upperResizeLimit=1000}) => {
+    const parentElement = React.useRef<HTMLHeadingElement>(null);
 
-            </span>
+    let [emToPx, setEmToPx] = React.useState(16); // By default
+    React.useEffect(() => {
+        if(parentElement.current != null){
+            setEmToPx(parseFloat(getComputedStyle(parentElement.current).fontSize));
+        }
+    }, []); // Is this going to be performant?
+    
+    return(
+        <h1 className={className} ref={parentElement}>
+            <ResponsiveResizer lowerResizeLimit={lowerResizeLimit} upperResizeLimit={upperResizeLimit} emToPx={emToPx}>
+                {children}
+            </ResponsiveResizer>
         </h1>
     );
 }
